1589e8359980961a88da348836195b5869b58511
karan <karankalai75@gmail.com>
2021-07-18 16:55:25 +0530

    create layout for shop app

--- Shop/urls.py
+++ Shop/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='ShopIndex')
+    path('', views.index, name='ShopIndex'),
+    path('about/', views.about, name='ShopAbout'),
+    path('contact/', views.contact, name='ShopContact'),
+    path('tracker/', views.tracker, name='ShopTracker'),
+    path('search/', views.search, name='ShopSearch'),
+    path('productView/', views.productView, name='ShopProductView'),
+    path('checkout/', views.checkout, name='ShopCheckout'),
 ]
--- Shop/views.py
+++ Shop/views.py
@@ -3,4 +3,22 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Shop Index')
+    return render(request, 'shop/index.html')
+
+def about(request):
+    return HttpResponse('We are at About Page')
+
+def contact(request):
+    return HttpResponse('We are at Contact Page')
+
+def tracker(request):
+    return HttpResponse('We are at Tracker Page')
+
+def search(request):
+    return HttpResponse('We are at Search Page')
+
+def productView(request):
+    return HttpResponse('We are at ProductView Page')
+
+def checkout(request):
+    return HttpResponse('We are at checkout Page')
